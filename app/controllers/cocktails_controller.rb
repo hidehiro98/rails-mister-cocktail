86e6ba96ed3a023060f0258f23605cbfff875b32
Hidehiro Nagaoka <kuinige@gmail.com>
2017-04-12 21:50:40 +0900

    fix strong params

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -34,7 +34,7 @@ class CocktailsController < ApplicationController
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :photo, :photo_cache)
+    params.require(:cocktail).permit(:name, :photo, :photo_cache, :address)
   end
 
   def set_cocktail
