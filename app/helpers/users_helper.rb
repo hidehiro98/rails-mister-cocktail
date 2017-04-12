3a92860bda25d97dbc5d6f511805f808a9010bac
Hidehiro Nagaoka <kuinige@gmail.com>
2017-04-12 21:32:44 +0900

    fix navbar profile photo error

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,13 @@
+module UsersHelper
+  def user_avatar_url(user)
+    if user
+      if user.facebook_picture_url.present?
+        user.facebook_picture_url
+      else
+        "http://placehold.it/30x30"
+      end
+    else
+      "http://placehold.it/30x30"
+    end
+  end
+end
