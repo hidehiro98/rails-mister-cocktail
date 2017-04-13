363a369a63513afaa7c8d54012fbee034671ab4b
Hidehiro Nagaoka <kuinige@gmail.com>
2017-04-13 21:54:50 +0900

    fix helper and navbar of profile pic

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   def user_avatar_url(user)
-    if user
+    # if user
       if user.facebook_picture_url.present?
         user.facebook_picture_url
       else
@@ -8,6 +8,6 @@ module UsersHelper
       end
     else
       "http://placehold.it/30x30"
-    end
+    # end
   end
 end
