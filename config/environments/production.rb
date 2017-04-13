a2a74367841fcb25f56417389e8792c742dd5468
Hidehiro Nagaoka <kuinige@gmail.com>
2017-04-13 11:21:14 +0900

    fix gmail_app_password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.default_url_options = { host: 'mr-cocktail-hidehiro98.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'https://mr-cocktail-hidehiro98.herokuapp.com/' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
