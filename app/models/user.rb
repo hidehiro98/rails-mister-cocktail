376c49f6f8a755cd2032f434669b635774d127fb
Hidehiro Nagaoka <kuinige@gmail.com>
2017-04-14 13:12:55 +0900

    test2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   after_create :send_welcome_mail
 
-  test in for git
+  test2 in for git
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
